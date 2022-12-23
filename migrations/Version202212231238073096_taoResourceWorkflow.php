9f49918c62b989e2533177bea7e4b7abcba2e8fa
rgomeztao <raul.gomez.cabello@taotesting.com>
2022-12-23 13:38:32 +0100

    feat: adds new empty migration

---
+++ migrations/Version202212231238073096_taoResourceWorkflow.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\taoResourceWorkflow\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version202212231238073096_taoResourceWorkflow extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
